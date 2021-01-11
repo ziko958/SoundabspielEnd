432c721701a8d67471ff4419262c292c0871610f
sfar aziz <sfaraziz@sfars-MBP.lan>
2021-01-11 12:41:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/soundabspiel/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.soundabspiel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Music> arrayList;
+    private CustomMusicAdapter adapter;
+    private ListView songList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        songList = (ListView) findViewById(R.id.songList);
+        arrayList = new ArrayList<>();
+
+
+        //Songs einfügen mit name singer und song
+        arrayList.add(new Music("","",1));
+
+        adapter = new CustomMusicAdapter(this,R.layout.custom_music_item, arrayList);
+        songList.setAdapter(adapter);
+
+    }
+}
